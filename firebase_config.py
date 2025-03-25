0859ff213ba8ef102cf30713bae5cbdb57fa89db
IcaroFeroliv <Icaroferreirabim@gmail.com>
2025-03-25 11:05:56 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,12 @@
 import firebase_admin
+import streamlit as st
 from firebase_admin import credentials, firestore
 
-# Caminho correto para o JSON
-cred_path = r"C:\Users\Usuario\PycharmProjects\Streamlit\bibliotecaprojeta-840dc-firebase-adminsdk-fbsvc-eef0722126.json"
+
 
 # Verificar se o Firebase já está inicializado
-if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
-    firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(st.secrets["firebase"])
+firebase_admin.initialize_app(cred)
 
 # Inicializar o Firestore
 db = firestore.client()
