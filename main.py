66158c665bdb2ba747719aa7f5f09f2c397de5c5
IcaroFeroliv <Icaroferreirabim@gmail.com>
2025-03-31 15:28:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-st.markdown("""
-<style>
-.dark {
-    background-color: #121212;
-    color: white;
-}
-</style>,unsafe_allow_html=True
-)
-
 # Corrigir quebra de linha da chave
 firebase_config = dict(st.secrets["firebase"])
 firebase_config["private_key"] = firebase_config["private_key"].replace("\\n", "\n")
