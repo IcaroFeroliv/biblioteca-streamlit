2cacfcade0c024f9f83dbf20d47c06b3e8e5482d
IcaroFeroliv <Icaroferreirabim@gmail.com>
2025-03-31 13:19:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide")
 
 st.markdown("""
 <style>
-.stMainMenu.st-emotion-cache-czk5ss.ev04twb8
+.stToolbarActions.st-emotion-cache-1p1m4ay.ekuhni82
 {
     visibility: hidden;
 }
